3920c2fbc6451b363a0a12cbc6178969615aa28e
Sofia <sofiabsb02@gmail.com>
2024-04-10 18:16:11 -0300

    Modificações do Rizzi

--- CubePrison/Assets/Scripts/CanvasController.cs
+++ CubePrison/Assets/Scripts/CanvasController.cs
@@ -1,17 +1,23 @@
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class CanvasController : MonoBehaviour
 {
     private static CanvasController instance;
 
-    public GameObject mainObject; // Objeto principal a ser controlado
-   // public GameObject[] additionalObjects; // Array de objetos adicionais a serem controlados
+    public int SelectedInInventory = 0, j;
 
-    private bool mainObjectVisible = false; // Flag para controlar a visibilidade do objeto principal
+    public GameObject mainObject; // Objeto principal a ser controlado
+
+    public bool mainObjectVisible = false, SavingInInventory, SavedSlot = false; // Flag para controlar a visibilidade do objeto principal
 
     public UIItem[] uiItems;
+
+    public int[] InventorySlot = new int[6];
 
     private void Awake()
     {
@@ -33,7 +39,6 @@ public class CanvasController : MonoBehaviour
         return instance;
     }
 
-
     void Update()
     {
         // Verificar se a tecla I foi pressionada
@@ -44,15 +49,25 @@ public class CanvasController : MonoBehaviour
             mainObject.SetActive(mainObjectVisible);
         }
         List<int> itensUsados = new();
+
         // Verificar se uma nova string foi salva no PlayerPrefs
         for (int i = 0; i < ItemSaver.GetInstance().stringList.Count; i++)
         {
-            for (int j = 0; j < uiItems.Length; j++)
+            for (j = 0; j < uiItems.Length; j++)
             {
                 if (ItemSaver.GetInstance().stringList[i] == uiItems[j].key)
                 {
                     uiItems[j].image.SetActive(true);
                     itensUsados.Add(j);
+                    uiItems[j].slot = j;
+
+                    if (!SavedSlot)
+                    {
+                        SavedSlot = true;
+                        InventorySlot[j] = j;
+                        SavedSlot = false;
+                    }
+                        
                 }
             }
         }
@@ -65,6 +80,14 @@ public class CanvasController : MonoBehaviour
             }
         }
     }
+
+    public void OnButtonClick(GameObject item)
+    {
+        int index = item.transform.GetSiblingIndex();
+        uiItems[index].selected = !uiItems[index].selected;
+        print("pinto");
+    }
+
 }
 
 [System.Serializable]
@@ -72,4 +95,7 @@ public class UIItem
 {
     public GameObject image;
     public string key;
+    public int slot;
+
+    public bool selected = false;
 }
